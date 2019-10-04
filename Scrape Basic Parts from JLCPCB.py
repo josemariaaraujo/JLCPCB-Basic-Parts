4b052849819dabd16df848f7b9226c2f67f9c509
josemariaaraujo <josemariaaraujo@gmail.com>
2019-10-04 11:38:19 +0100

    Compatibility with github CSV

--- Scrape Basic Parts from JLCPCB.py
+++ Scrape Basic Parts from JLCPCB.py
@@ -6,7 +6,7 @@ import csv
 conn= http.client.HTTPSConnection("jlcpcb.com")
 headers = {'Content-type': 'application/x-www-form-urlencoded'}
 with open('Basic Parts.csv', 'w', newline='') as csv_file:
-    component_writer = csv.writer(csv_file, delimiter=';')
+    component_writer = csv.writer(csv_file, delimiter=',')
     component_writer.writerow(['Manufacturer','LCSC Part #','Description','Package','Stock','Type','MFR.Part #',
                                       'Datasheet','Price (USD)'])
     for n in range(1,8):
@@ -19,6 +19,6 @@ with open('Basic Parts.csv', 'w', newline='') as csv_file:
             if component['componentLibraryType']=='base':
                 component_writer.writerow([component['componentBrandEn'],component['componentCode'],component[ 'describe'],component['componentSpecificationEn']+"\t",
                                             component['stockCount'],component['componentTypeEn'],component['componentModelEn'],component['dataManualUrl'],
-                                            str(component['componentPrices'][0]['productPrice']).replace('.',',')])      
+                                            component['componentPrices'][0]['productPrice']])      
 conn.close()
 
